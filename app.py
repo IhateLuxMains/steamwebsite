77fb547b950bd578732a827774792d9aa629ba31
Julian <Delpjul03>
2022-10-12 16:00:47 -0700

    take variable through html pass to python id var

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
+
+test =1234
+
+
+@app.route('/stats', methods=['GET', 'POST'])
+def foo():
+    steamID = request.form['steamID']
+    return steamID
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run()
--- webScrape.py
+++ webScrape.py
@@ -1,3 +1,4 @@
+
 from re import S
 from bs4 import BeautifulSoup
 import requests
@@ -7,9 +8,12 @@ key = "C:\\Users\\Julian\\Desktop\\steamAPIkey\\sAPIkey.txt"
 with open(key, 'r') as file:
     key = file.read()
 
-id =  76561198405626506
+def cause_app_to_do_something():
+    import app
+    id =  app.steamID
+
     # FIX KEY IN API HTML, OPEN FROM DOC 
-page = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=FCA53F323070D2EF17EF9D969B43EAC4&steamid=76561198401753028&include_appinfo=true&format=json" # % (key, id)
+page = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=FCA53F323070D2EF17EF9D969B43EAC4&steamid=steamID&include_appinfo=true&format=json" # % (key, id)
 soup = BeautifulSoup(requests.get(page).text, "html.parser")
 
 data = json.loads(soup.text)['response']['games']
@@ -20,7 +24,8 @@ for i in data:
             gamez.append(i['name'])
             time.append(i['playtime_forever'])
 
-print(gamez)
+def index():
+    return (gamez)
 print(time)
     
 
