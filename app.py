52ebc7d8466a977fb8cfb91a7babadd8ef4e86ac
Julian <Delpjul03>
2022-10-19 23:19:54 -0700

    site now displays api call of everything

--- app.py
+++ app.py
@@ -1,18 +1,30 @@
+
 from flask import Flask, request, render_template
+import requests
 
 app = Flask(__name__)
 
-test =1234
 
 
-@app.route('/stats', methods=['GET', 'POST'])
-def foo():
-    steamID = request.form['steamID']
-    return steamID
 
 @app.route('/')
-def index():
+def my_form():
     return render_template('index.html')
 
+
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    if request.method == 'POST':
+        id = request.form.get('variable')
+        response  = requests.get("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=FCA53F323070D2EF17EF9D969B43EAC4&steamid=%s&include_appinfo=true&format=json"% (id)).text
+        return response
+    
+   
 if __name__ == '__main__':
     app.run()
+
+
+
+
+    
--- gameInfo.py
+++ gameInfo.py
@@ -2,11 +2,21 @@ from urllib import response
 import requests
 import json
 import app
+import numpy as np
 
 default = 76561198405626506
-id = default
 
-response  = requests.get("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=FCA53F323070D2EF17EF9D969B43EAC4&steamid=%s&include_appinfo=true&format=json"% (id)).text
-response_info = json.loads(response)
 
-print(response_info)
+app.variable
+
+# response  = requests.get("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=FCA53F323070D2EF17EF9D969B43EAC4&steamid=%s&include_appinfo=true&format=json"% (id)).text
+# response_info = json.loads(response)['response']['games']
+
+# gamez = []
+# time = []  
+# for i in response_info:
+#         if(i['playtime_forever'] > 4000): #comparasion in mnutes
+#             gamez.append(i['name'])
+#             time.append(i['playtime_forever'])
+ 
+# np.column_stack((gamez,time))
